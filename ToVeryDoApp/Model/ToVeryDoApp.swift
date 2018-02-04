bd17a8925e1fd29275d902643029b1764b1a39bf
stefan-vm <stefan-vm@stefan-vms-Mac.local>
2018-02-04 16:53:38 +0100

    first

---
+++ ToVeryDoApp/Model/ToVeryDoApp.swift
@@ -0,0 +1,9 @@
+//
+//  ToVeryDoApp.swift
+//  ToVeryDoApp
+//
+//  Created by stefan-vm on 04/02/2018.
+//  Copyright © 2018 stefan-vm. All rights reserved.
+//
+
+import Foundation
---
+++ ToVeryDoApp/View/OnBoardingView.swift
@@ -0,0 +1,13 @@
+//
+//  OnBoardingView.swift
+//  ToVeryDoApp
+//
+//  Created by stefan-vm on 04/02/2018.
+//  Copyright © 2018 stefan-vm. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingView: PaperOnboarding {
+
+}
